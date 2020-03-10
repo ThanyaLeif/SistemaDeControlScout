625bb0880b6bef1407699f7b6066c39302c74464
ThanyaLeif <32908302+ThanyaLeif@users.noreply.github.com>
2020-03-10 17:43:03 -0600

    Se agrego la funcionalidad de cambiar de tab en la vista del scout.

--- app/src/main/java/com/example/tanialeif/sistemadecontrolscout/MenuScout/MenuPrincipalScout.java
+++ app/src/main/java/com/example/tanialeif/sistemadecontrolscout/MenuScout/MenuPrincipalScout.java
@@ -1,15 +1,48 @@
 package com.example.tanialeif.sistemadecontrolscout.MenuScout;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.tanialeif.sistemadecontrolscout.R;
 
+import java.time.Instant;
+
 public class MenuPrincipalScout extends AppCompatActivity {
+
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal_scout);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPagerScout);
+        loadViewPager(viewPager);
+        tabLayout = (TabLayout)findViewById(R.id.tabsScout);
+        tabLayout.setupWithViewPager(viewPager);
+        tabNames();
+    }
+
+    private void tabNames(){
+        tabLayout.getTabAt(0).setText("Información");
+        tabLayout.getTabAt(1).setText("Insignias");
+    }
+
+    private void loadViewPager(ViewPager viewPager){
+        ViewPagerAdapterScout adapterScout = new ViewPagerAdapterScout(getSupportFragmentManager());
+        adapterScout.addFragment(newInstance("info"));
+        adapterScout.addFragment(newInstance("insignias"));
+        viewPager.setAdapter(adapterScout);
+    }
+
+    private TabInfoFragment newInstance(String title){
+        Bundle bundle = new Bundle();
+        bundle.putString("title", title);
+        TabInfoFragment tabInfoFragment = new TabInfoFragment();
+        tabInfoFragment.setArguments(bundle);
+
+        return tabInfoFragment;
     }
 }
---
+++ app/src/main/java/com/example/tanialeif/sistemadecontrolscout/MenuScout/ViewPagerAdapterScout.java
@@ -0,0 +1,31 @@
+package com.example.tanialeif.sistemadecontrolscout.MenuScout;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerAdapterScout extends FragmentPagerAdapter {
+
+    List <Fragment> fragmentList = new ArrayList<>();
+
+    public ViewPagerAdapterScout(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return fragmentList.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment){
+        fragmentList.add(fragment);
+    }
+}
